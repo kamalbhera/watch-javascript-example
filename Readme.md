74d753eb2d55c999bd6626fee04fd696e21a1dde
kamlesh <kamlesh.kumawat@axa-abs-co.in>
2023-01-10 20:50:57 +0530

    initial commit

---
+++ Readme.md
@@ -0,0 +1,22 @@
+## Javascript Awesome Method and Basic point to help interview
+
+A Theme for admin panel.
+
+## License
+
+This source code is licensed under the MIT license.
+
+## Author
+
+<table>
+  <tr>
+    <td>
+      <img src="https://avatars.githubusercontent.com/u/65757787?v=4" width="100">
+    </td>
+    <td>
+      CS Kamal Bhera<br />
+      <a href="mailto:kamalbhera96@gmail.com">kamalbhera96@gmail.com</a><br />
+      Instagram : <a href="https://www.instagram.com/cskamalbhera/">cskamalbhera</a>
+    </td>
+  </tr>
+</table>
---
+++ index.js
@@ -0,0 +1,58 @@
+const hour = document.getElementById('hour');
+const minute = document.getElementById('minute');
+const second = document.getElementById('second');
+const hand = document.getElementsByClassName('hand');
+const digitalHour = document.getElementById('digitalHour');
+const digitalMinute = document.getElementById('digitalMinute');
+const digitalSecond = document.getElementById('digitalSecond');
+
+let min = 0;
+let sec = 0;
+let h = 0;
+
+const twoDigitNum = (num) => {
+    if (num < 10) {
+      return `0${num}`;
+    }
+    return num;
+  };
+
+function clock() {
+    const date = new Date();
+    sec = date.getSeconds() * 6;
+    if (sec === 0) {
+      second.style.transition = "none";
+    } else {
+      second.style.transition = ".4s all ease-in";
+    }
+  
+    if (min === 0) {
+      min = date.getMinutes() * 6;
+    }
+  
+    // movement of minute needle in one seconde
+    // (total rotation / total minutes round) / total second in a minute
+    // (360 / 60) / 60
+    min += 6 / 60;
+  
+    if (h === 0) {
+      h = date.getHours() * 30;
+    }
+  
+    // movement of hour needle in one second
+    // (total rotation / total rounds (12)) / total second in an hour
+    // (360 / 12) / (60 * 60)
+    h += 30 / (60 * 60);
+  
+    second.style.transform = `rotate(${sec}deg)`;
+    minute.style.transform = `rotate(${min}deg)`;
+    hour.style.transform = `rotate(${h}deg)`;
+    digitalHour.innerHTML = twoDigitNum(date.getHours());
+    digitalMinute.innerHTML = twoDigitNum(date.getMinutes());
+    digitalSecond.innerHTML = twoDigitNum(date.getSeconds());
+  }
+
+  clock();
+  setInterval(() => {
+    clock()
+  }, 1000);
